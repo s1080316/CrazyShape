8ef787b9efce093a79c490b5cdf3a240986c65c4
0521 <s1080316@gm.pu.edu.tw>
2021-05-21 15:11:48 +0800

    2021上機考v1_(吳淑芬)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -3,15 +3,16 @@ package tw.edu.pu.s1080316.crazyshape
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import tw.edu.pu.csim.tcyang.crazyshape.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Toast.makeText(baseContext, "作者：吳淑芬", Toast.LENGTH_LONG).show()
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
@@ -21,5 +22,15 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        var PictureNo:Int = 0  //目前顯示第幾張圖
+        var TotalPictures:Int = 4 //總共幾張圖片(假設僅顯示pu0-pu3)
+
+
+
+        }
+
+
     }
+
 }
